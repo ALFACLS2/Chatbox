3edf98081fd7bf579baed1a6d0ccf5d06b3bb464
ALFACLS2 <anggoro.sasmito@sat.co.id>
2026-03-29 22:22:54 +0700

    Create knowledge.js

---
+++ data/knowledge.js
@@ -0,0 +1,6 @@
+const knowledge = {
+  "cuti": "Pengajuan cuti dilakukan melalui HRIS.",
+  "izin": "Pengajuan izin dilakukan melalui HRIS.",
+  "resign": "Ajukan resign minimal 30 hari sebelumnya.",
+  "gaji": "Gaji dibayarkan setiap akhir bulan."
+};
